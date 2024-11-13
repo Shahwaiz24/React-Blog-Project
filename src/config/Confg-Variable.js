060f392fc0c83ee688613c733b57a16f3b1222de
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-14 00:53:31 +0500

    Updated and Created Config

--- src/config/Confg-Variable.js
+++ src/config/Confg-Variable.js
@@ -1,1 +1,10 @@
-const conf
+const config = {
+    BACKEND_URL: String(import.meta.env.VITE_BACKEND_URL),
+    BACKEND_DATABASE_ID: String(import.meta.env.VITE_BACKEND_URL),
+    BACKEND_PROJECT_ID: String(import.meta.env.VITE_BACKEND_URL),
+    BACKEND_BUCKET_ID: String(import.meta.env.VITE_BACKEND_URL),
+    BACKEND_COLLECTION_ID: String(import.meta.env.VITE_BACKEND_URL),
+
+
+}
+export default config;
