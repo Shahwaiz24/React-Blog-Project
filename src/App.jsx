188441f13bd372341600e17fed12c74c2587ec7a
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-18 22:25:38 +0500

    Loader Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import {signUp,login,logout} from "./services/Slices/Auth-Slices";
 import { useDispatch, useSelector } from "react-redux";
+import LoaderComponent from "./component/loader/loader";
 
 function App() {
 
@@ -10,26 +11,30 @@ function App() {
   let status = useSelector((state) => state.auth.status)
 
   useEffect(() => {
-    let isLogin = true;
-      // JSON.parse(localStorage.getItem("isLogin")) || true;
+    let isLogin = JSON.parse(localStorage.getItem("isLogin")) || false
     if (isLogin == true) {
     
 
       let userId = JSON.parse(localStorage.getItem("userId")) || {};
       // Api Fetch Etc //
       dispatch(login({ userData: {} }));
-      console.log("Login", " status: ",status)
+      console.log("Login", " status: ", status)
+      setLoading(false)
     } else {
       console.log("Logout")
 
-      logout();
+      dispatch(logout()) 
+      setLoading(false)
+
     }
   },[])
 
 
   return (
     <>
-      <h1>{status.toString()}</h1>
+      {
+        isloading == true ? <LoaderComponent /> : <h1>{status.toString()}</h1>
+      }
     </>
   )
 }
