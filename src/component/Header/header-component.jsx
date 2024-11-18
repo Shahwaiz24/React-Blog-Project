8b08020d23e8cd25509c8d8d47759f22d32c6f2c
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-19 02:08:50 +0500

    Navbar Completed just 1 or 2 things left

--- src/component/Header/header-component.jsx
+++ src/component/Header/header-component.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-
-import { searchIcon, shoppingBag, userCircle } from './index';
+import { mobileIcon, closeIcon,searchIcon,userCircle,shoppingBag } from './index';  // Make sure your icons are correctly imported
 
 export default function HeaderComponent() {
     const [open, setOpen] = useState(false);
-    const location = useLocation(); // To get the current path
+    const location = useLocation(); 
 
-    // Function to check if the link is active
+    const [isMobile, setMobile] = useState(false);
+
     const isActive = (path) => {
         return location.pathname === path ? 'text-customNavGreen' : 'hover:text-customNavGreen';
     };
+    const isInMobile = () => {
+        setMobile(true)
+    }
 
     return (
         <div className="min-h-screen">
             <div className="antialiased bg-gray-100">
                 <div className="w-full text-gray-700 bg-white">
-                    {/* This flex container will keep the layout in row for all screen sizes */}
-                    <div className="flex justify-between items-center max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-
+                    <div className={`flex justify-between items-center max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8 pt-5`}>
                         {/* Left section - 3legant text */}
                         <div className="flex items-center justify-start">
                             <Link to="/" className="text-xl font-poppins font-medium tracking-widest text-black rounded-lg focus:outline-none focus:shadow-outline">
@@ -46,25 +47,66 @@ export default function HeaderComponent() {
                         <div className="flex gap-2 items-center justify-end hidden md:flex">
                             <Link to="/">
                                 <img src={searchIcon} alt="Search Icon" className={`w-5 h-5 md:w-6 md:h-6`} />
-                            
-                            </Link >
+                            </Link>
                             <Link to="/">
                                 <img src={userCircle} alt="User Icon" className="w-5 h-5 md:w-6 md:h-6" />
                             </Link>
                             <Link to="/">
-                                <img src={shoppingBag} alt="Shopping Bag Icon" className="w-5 h-5 md:w-6 md:h-6" /></Link>
+                                <img src={shoppingBag} alt="Shopping Bag Icon" className="w-5 h-5 md:w-6 md:h-6" />
+                            </Link>
                         </div>
 
                         {/* Mobile menu button */}
-                        <button className="rounded-lg md:hidden focus:outline-none focus:shadow-outline" onClick={() => setOpen(!open)}>
-                            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
-                                <path x-show="!open" fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                                <path x-show="open" fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
-                            </svg>
+                        <button
+                            className="rounded-lg md:hidden focus:outline-none focus:shadow-outline "
+                            onClick={() => setOpen(true)}
+                            // style={{ paddingTop: "10px"}}
+                        >
+                            {/* Display the mobile icon or close icon based on the 'open' state */}
+                            <img
+                                src={open ? closeIcon : mobileIcon}  // Dynamically change icon based on state
+                                alt="Menu Icon"
+                                className="w-6 h-6"
+                            />
                         </button>
                     </div>
                 </div>
             </div>
+
+            {/* Mobile menu content */}
+            {open && (
+                <div className="absolute top-0 left-0 w-full h-full bg-white p-4 md:hidden">
+                    <div className="flex justify-end">
+                        <button
+                            className="rounded-lg md:hidden focus:outline-none focus:shadow-outline"
+                            onClick={() => setOpen(false)}
+                            style={{ paddingTop: "10px" }}
+                        >
+                            {/* Display the mobile icon or close icon based on the 'open' state */}
+                            <img
+                                src={ closeIcon }  // Dynamically change icon based on state
+                                alt="Menu Icon"
+                                className="w-5 h-5"
+                            />
+                        </button>
+                    </div>
+
+                    <div className="flex flex-col items-center space-y-5 pt-20">
+                        <Link to="/" className={`text-sm font-poppins font-light text-black ${isActive('')}`}>
+                            Home
+                        </Link>
+                        <Link to="/" className={`text-sm font-poppins font-light text-black ${isActive('')}`}>
+                            Shop
+                        </Link>
+                        <Link to="/" className={`text-sm font-poppins font-light text-black ${isActive('')}`}>
+                            Product
+                        </Link>
+                        <Link to="/" className={`text-sm font-poppins font-light text-black ${isActive('')}`}>
+                            Contact Us
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,9 +1,14 @@
 import searchIcon from './nav-icons/search-02.png';
 import shoppingBag from './nav-icons/shopping bag.png';
 import userCircle from './nav-icons/user-circle.png';
+import mobileIcon from './nav-icons/hamburger.png';
+import closeIcon from './nav-icons/close.png';
+
 
 export {
     searchIcon,
     shoppingBag,
-    userCircle
+    userCircle,
+    mobileIcon,
+    closeIcon
 }
