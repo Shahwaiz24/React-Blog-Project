c426810b58811ee17edd6841f2a0add53d3f62b7
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-19 18:02:57 +0500

    Foam Completed The Api Integrations and BackEnd Configure will be in Last

--- src/component/Login Foam/login-foam-component.jsx
+++ src/component/Login Foam/login-foam-component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link} from "react-router-dom";
 import InputFieldsComponent from "../Input Fields/input-fields-component";
 import { ButtonComponent } from "../Home";
 
@@ -46,9 +47,23 @@ export default function LoginFoamComponent() {
                 {/* Submit Button */}
                 <ButtonComponent type="submit" className="w-full bg-black text-black hover:text-white py-2 rounded-md hover:bg-gray-800 transition-all duration-200" children={<h1 className="font-poppins font-medium ">Login</h1>}>
                 </ButtonComponent>
+                <div className={`flex justify-between px-2 pt-4`}>
+                    <Link to={"/"}> <h1 className="font-poppins font-medium text-sm hover:text-customNavGreen">Forgot Password?</h1></Link>
+                    <Link to={"/"}>  <h1 className="font-poppins font-medium text-sm hover:text-customNavGreen">Sign Up</h1></Link>
+
+
+                </div>
+                <div className="mt-5 flex text-center justify-center text-sm text-gray-400">
+                    <p>
+                        This site is protected by reCAPTCHA and the Google <br />
+                        <Link to={"/"} className="underline">Privacy Policy</Link>  and <Link to={"/"} className="underline" href="">Terms of Service</Link>  apply.
+                    </p>
+                </div>
+                
 
                
             </form>
+           
         </div>
     );
 }
