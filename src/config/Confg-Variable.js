5f978cc569133a2f5f0ddaac4ae769fda24763cb
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-22 17:16:55 +0500

    ENV Variable Connected

--- src/config/Confg-Variable.js
+++ src/config/Confg-Variable.js
@@ -1,9 +1,11 @@
 const config = {
     BACKEND_URL: String(import.meta.env.VITE_BACKEND_URL),
-    BACKEND_DATABASE_ID: String(import.meta.env.VITE_BACKEND_URL),
-    BACKEND_PROJECT_ID: String(import.meta.env.VITE_BACKEND_URL),
-    BACKEND_BUCKET_ID: String(import.meta.env.VITE_BACKEND_URL),
-    BACKEND_COLLECTION_ID: String(import.meta.env.VITE_BACKEND_URL),
+    BACKEND_DATABASE_ID: String(import.meta.env.VITE_DATABASE_ID),
+    BACKEND_PROJECT_ID: String(import.meta.env.VITE_PROJECT_ID),
+    BACKEND_BUCKET_ID: String(import.meta.env.VITE_BUCKET_ID),
+    BACKEND_PRODUCT_COLLECTION_ID: String(import.meta.env.VITE_PRODUCT_COLLECTION_ID),
+    BACKEND_USER_COLLECTION_ID: String(import.meta.env.VITE_USER_COLLECTION_ID),
+
 
 
 }
--- src/pages/Login-Page.jsx
+++ src/pages/Login-Page.jsx
@@ -1,16 +1,11 @@
 import React, { Suspense } from "react";
 
-// Using lazy() to dynamically import LoginFoamComponent
-const LoginFoamComponent = React.lazy(() => 
-  import('../index.js').then(module => ({ default: module.LoginFoamComponent }))
-);
+import {LoginFoamComponent} from "../index"
+
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div className="w-full h-full flex flex-col justify-center">
-       <h1>Loading...</h1>
-    </div>}>
       <LoginFoamComponent />
-    </Suspense>
+
   );
 }
