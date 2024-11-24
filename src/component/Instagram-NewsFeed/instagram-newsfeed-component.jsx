5f92d78970df11d77c8d5723250043d9ec8a7640
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-24 22:18:25 +0500

    Instagram Feed Section Started

---
+++ src/component/Instagram-NewsFeed/instagram-newsfeed-component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NewsFeedPic from "../Instagram-NewsFeed/instagram-feed-images/last-section-img.png"
+
+export default function InstagramNewsFeedComponent({img = NewsFeedPic}){
+
+    return (
+        <div className="w-full h-full">
+
+        </div>
+    );
+}
