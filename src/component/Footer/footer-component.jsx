690cf6422b975f400e4d0a9a8edce27bce6607fa
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-24 22:47:50 +0500

    Footer Creating Starting

--- src/component/Footer/footer-component.jsx
+++ src/component/Footer/footer-component.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function FooterComponent() {
     return (
-        <div className="h-10">
-            <h1>Footer</h1>
+        <div className="w-full h-full bg-black px-4 sm:px-8 md:px-16 lg:px-24 xl:px-40 pt-20 flex justify-between">
+            <div className="text-start ">
+                <h1 className="text-white font-poppins font-medium text-xl">3legant.
+                </h1>
+                <h1 className="pt-5 text-white font-poppins font-medium text-xl">
+                More than just a game. <br /> It’s a lifestyle.
+                </h1>
+                <div className="pt-8"></div>
+
+            </div>
+
+           
         </div>
     );
 }
